119f35975baf706ffb97b7fa3c30c6ecc4edc0c2
Yui1022 <g14918ky+git@gm.tsuda.ac.jp>
2016-04-13 12:26:51 +0900

    Revert "文を追加しました"

--- NetBeansProjects/HelloWorldGit/src/helloworldgit/HelloWorldGit.java
+++ NetBeansProjects/HelloWorldGit/src/helloworldgit/HelloWorldGit.java
@@ -17,7 +17,6 @@ public class HelloWorldGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hello,world");
-        System.out.println("Hello");
     }
     
 }
